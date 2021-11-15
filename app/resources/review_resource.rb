843c075119412078240c7a5ced483fb142d0c265
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-11-15 16:58:06 +0000

    Direct association between UserResource and ReviewResource

--- app/resources/review_resource.rb
+++ app/resources/review_resource.rb
@@ -9,6 +9,8 @@ class ReviewResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :movie
 
   # Indirect associations
--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -8,6 +8,8 @@ class UserResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :reviews
+
   # Indirect associations
 
 end
