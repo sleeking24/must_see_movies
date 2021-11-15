6f22c6957920647507e6efb4a89de8a930c819c2
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-11-15 16:56:59 +0000

    Direct association between users and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   belongs_to :movie
 
   # Indirect associations
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Review, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:user) }
+
     it { should belong_to(:movie) }
 
     end
