203df4b69935e97d9deb681645584dfb18f7b94e
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-11-15 16:57:58 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,16 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :image, :string
+  attribute :description, :string
+  attribute :director_id, :integer
+  attribute :year, :date
+  attribute :duration, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
